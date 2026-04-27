b490f59dec7c92a43b139871398e9c924741bc58
TanGuoliang <1445360005@qq.com>
2026-04-27 22:39:14 +0800

    项目后端初始化

---
+++ src/test/java/com/tangl/aistocksimulatorbackend/AiStockSimulatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangl.aistocksimulatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStockSimulatorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
